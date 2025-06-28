fcdedad7e9d590cf513727148e466d4c9e7c2979
Chinedu Pius <nexafortis@gmail.com>
2025-06-29 09:56:59 +1000

    Fix Tailwind output

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
-import React from 'react'; import ReactDOM from 'react-dom/client'; import './output.css'; import NexaFortisLanding from './NexaFortisLanding'; const root = ReactDOM.createRoot(document.getElementById('root')); root.render(<NexaFortisLanding />);
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './output.css'; // <-- this is the compiled version
+import NexaFortisLanding from './NexaFortisLanding';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<NexaFortisLanding />);
